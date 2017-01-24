7c6b0027283911dc2429e407f49a3cd4979871ba
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2017-01-23 21:02:27 -0600

    Updates

--- libs/scripts/home/app.sessions.js
+++ libs/scripts/home/app.sessions.js
@@ -361,6 +361,14 @@
                         ['Rotators', 'sessions/31.html'],
                     ]
                 },
+                {
+                    num: 33,
+                    date: '1/24/2017',
+                    links: [
+                        ['Modals', 'sessions/32.html'],
+                    ]
+                },
+
 
 
 
