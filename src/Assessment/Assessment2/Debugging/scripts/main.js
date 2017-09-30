aac6060b7f50f5144ae402c629feee4cb01cf152
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2017-09-30 05:08:10 -0500

    Updates to assessment

--- src/Assessment/Assessment2/Debugging/scripts/main.js
+++ src/Assessment/Assessment2/Debugging/scripts/main.js
@@ -1,5 +1,94 @@
+/*
+    Read the comments and follow the instructions
+    Total Points available -> 70
+    Total Bonus points -> 15
+*/ 
+
+
 /*========================
     Part 1 Writing Functions -  10 points each
+====================================*/
+
+
+/*
+    The code below is broken and not doing what it should.  
+    
+    Do your best to fix it.
+    Look for:
+    Wrong selectors
+    Misspelling
+    Variables used at the wrong time
+    Wrong operators
+
+    It uses the HTML in /debugging/index.html  
+
+    Points Available: 30points
+*/
+$(function () {
+
+
+    //This is supposed to add an event listener but it's causing an error
+    //Fix it please.
+    //document.getElementById('#btnMake').addEventListener('click', build);
+
+    function build() {
+        var firstName = $('.firstName');
+        var firstNameValue = firstName.value;
+
+
+        var lastName = $('.lastName');
+        var lastNameValue = lastName.value;
+
+        /*invalid should add a red border around the respective input
+            It will also show a hidden error message */
+        if (firstNameValue = '') {
+            lastName.addClass('invalid');
+
+            var parentDiv = firstname.closets('#input-group');
+            var errorSpan = parentDiv.find('errors');
+            errorSpan.show();
+        }
+
+        if (lastNameValue = '') {
+            lastName.addClass('invalid');
+
+            var parentDiv = firstname.closets('#input-group');
+            var errorSpan = parentDiv.find('errors');
+            errorSpan.show();
+        }
+
+
+        /*====================================
+            I need to create an object, but it's not working
+            so I just commented it out.
+            Please fix it
+        ======================================*/
+        /*
+        var userObject = {
+            firstName = firstName;
+            lastName = lastName;
+            fullname = function () {
+                firstname + ' ' + lastName;
+            };
+            state = stateValue;
+        };
+
+        console.log(userObject);
+        */
+
+
+    }
+
+
+
+
+
+});
+
+
+
+/*========================
+    Part 2 Writing Functions -  10 points each
 ====================================*/
 
 
@@ -13,7 +102,6 @@
     console.log(foo) // Outputs 11
 
 */
-
 
 
 
@@ -40,5 +128,119 @@
     var foo2 = percentage(.5);
     console.log(foo2) //Outputs "50%"
 
+*/
+
+
+/*
+    Write a JavaScript function that takes a date and adds a 
+    number of days to it.
+
+    //Sample
+    var d = new Date('1/1/2001');
+    var newD = addDays(d, 10);
+    console.log(newD);  //outputs Thu Jan 11 2001 00:00:00 GMT-0600 (Central Standard Time)
+*/
+
+
+
+
+/*
+    BONUS
+    Write a javascript function to take an array of numbers
+    and sorts them in ascending order.
+
+    //Sample
+    var array1 = [3, 5, 1, 10];
+    console.log(sortArray(array1); //outputs [1,3,5,10]
+
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+=====================================================================
+    Nothing is broken here... here be dragons
+    Go away :)
+*/
+$(function () {
+    $('#btnReset').on('click', function () {
+        $('#firstName').val('John');
+        $('#lastName').val('Doe');
+        $('#state').val('OK');
+        $('.errors').hide();
+        $('.invalid').removeClass('invalid');
+    });
+});
--- src/Assessment/Assessment2/HTMLForm/ReadMe.md
+++ src/Assessment/Assessment2/HTMLForm/ReadMe.md
@@ -1,4 +1,4 @@
-# Assessment 2
+# Assessment 2 - HTML Forms 
 In this assignment we are going to be using everything we have learned so far with JavaScript, HTML and CSS.  You are to finish building out this page, wire up JavaScript, and style it the best you can with CSS.
 
 Do your best to recreate the following page. [final.png](final.png)
@@ -6,7 +6,7 @@ Do your best to recreate the following page. [final.png](final.png)
 When you complete the project, upload your changes to GitHub and let the instructor know.
 
 ## HTML Requirements
-- Use labels and inputs to finish the page. 
+- Use buttons, labels and inputs to finish the page. 
 
 ## CSS Requirements
 Use the CSS we have covered to best recreate the form and layout of the page.
@@ -20,7 +20,7 @@ Use JavaScript to enforce the following rules.  This is exactly like what we hav
 - Email must contain '@' and '.com'
 - A value must be selected in the 'How did you find us" drop down
 - 'Why do you need this blender' must be filled out
-- The terms and service checkbox must be checked.
+- The terms and service checkbox must be checked.  *We didn't cover this in jQuery, use Vanilla Javascript!!*
 - Add a class to any invalid input field.
 - Any error messages should be displayed above the form.
 
