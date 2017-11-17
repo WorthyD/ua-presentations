6bd0ebd8d59898023c6a792a6e519e15045ca92c
Daniel Worthy <danieljworthy@gmail.com>
2017-11-17 06:17:52 -0600

    Updates

--- src/Assessment/Assessment3/javascript/main.js
+++ src/Assessment/Assessment3/javascript/main.js
@@ -17,11 +17,19 @@ var humans = [];
 
 
 
+
+test('q1', humans);
+
+
+
 /*==============
     Q2 -  Use JavaScript to retrieve the object in the array with the 'id' equal to 2.
 ================*/
 var leia;
 
+
+
+test('q2', leia);
 
 /*==============
     Q3 - Use JavaScript to calculate the total age of all users.    
@@ -29,15 +37,47 @@ var leia;
 var totalAge = 0;
 
 
+
+
+
+test('q3', totalAge);
 /*==============
-    Q4 - Use JavaScript to sort this array of numbers ascending.
+    Q4 - Use JavaScript to create a new array that contains only the username of each user
+    Expected Output: userNames = ["luke.sky", "leia.org", "han.solo", "chewy"]
+================*/
+var userNames = [];
+
+
+
+
+test('q4', userNames);
+
+/*==============
+    Q5 - Use JavaScript to sort this array of numbers ascending.
 ================*/
 var numbers = [15,16,8,4,23,42]
 
+
+
+test('q5', numbers);
+
 /*==============
-    Q5 - Use JavaScript to replace all 
+    Q6 - Use JavaScript to replace all instances of the word 'lorem' in this string using regular expressions
 ================*/
-var replacement = '';
+var replacement = 'Lorem ipsum dolor sit lorem consectetur adipisicing lorem. Numquam, dicta repellendus excepturi consequatur sint ipsum quibusdam delectus lorem laborum eveniet fuga officiis nesciunt nemo ab dignissimos eos doloremque consectetur quod praesentium reprehenderit. Incidunt voluptate, quo rerum mollitia adipisci nam dignissimos, ex cupiditate accusamus cumque sunt corrupti lorem vlorem nemo explicabo.';
+
+
+test('q6', replacement);
+
+
+/*==============
+    Q7 - Use JavaScript to replace all instances of the word 'lorem' in this string using regular expressions
+================*/
 
 
 
+
+
+function test(id,result){
+    document.getElementById(id).innerHTML = JSON.stringify(result);
+}
