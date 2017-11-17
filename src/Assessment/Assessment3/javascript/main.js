ca9263db2d3a99db5f95e944b124ba44bceff40a
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2017-11-17 04:30:25 -0600

    Updates

---
+++ src/Assessment/Assessment3/javascript/main.js
@@ -0,0 +1,43 @@
+/*=========================
+    JavaScript Array Methods
+=========================*/
+
+/* Use this array for Q1 through Q3 */
+var users = [
+    {id:1, username: 'luke.sky', firstName: 'Luke', age: 20, race: 'human'},
+    {id:2, username: 'leia.org', firstName: 'Leia', age: 20, race: 'human'},
+    {id:3, username: 'han.solo', firstName: 'Han', age: 25, race: 'human'},
+    {id:4, username: 'chewy', firstName: 'Chewbacca', age:200, race: 'wookie'} 
+];
+
+/*==============
+    Q1 -  Use JavaScript to retrieve all the 'humans' in the array.
+================*/
+var humans = [];
+
+
+
+/*==============
+    Q2 -  Use JavaScript to retrieve the object in the array with the 'id' equal to 2.
+================*/
+var leia;
+
+
+/*==============
+    Q3 - Use JavaScript to calculate the total age of all users.    
+================*/
+var totalAge = 0;
+
+
+/*==============
+    Q4 - Use JavaScript to sort this array of numbers ascending.
+================*/
+var numbers = [15,16,8,4,23,42]
+
+/*==============
+    Q5 - Use JavaScript to replace all 
+================*/
+var replacement = '';
+
+
+
